7f75802abcb1dfd859f124c285dda3b26e22d1ee
HAN <gksrlgjsz@magice.co>
2021-03-11 16:34:08 +0900

    magicecole 디자인 파일 0311 업데이트

--- assets/js/dashboard-four.js
+++ assets/js/dashboard-four.js
@@ -82,13 +82,13 @@ $(function(){
   var chartBar = new Chart(ctx2, {
     type: 'horizontalBar',
     data: {
-      labels: ['Modification', 'Code Request', 'Feature Request', 'Bug Fix', 'Integration', 'Production'],
+      labels: ['MISSION 01', 'MISSION 02', 'MISSION 03', 'MISSION 04'],
       datasets: [{
-        data: [90, 60, 50, 95, 50, 60],
-        backgroundColor: ['#65e0e0', '#69b2f8','#6fd39b','#f77eb9','#fdb16d','#c693f9']
-      },{
-        data: [60, 50, 70, 45, 70, 30],
+        data: [90, 60, 50, 95],
         backgroundColor: '#e5e9f2'
+      },{
+        data: [60, 50, 70, 45],
+        backgroundColor: '#0079E4'
       }]
     },
     options: {
